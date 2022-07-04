f22d55868199099fb29867e927a30f640896d76d
Ondrej Kollar <ondrej.kollar@ibm.com>
2022-07-04 10:13:51 +0200

    Website_skeleton

--- javascript/wrapper-animations.js
+++ javascript/wrapper-animations.js
@@ -23,3 +23,26 @@ $('.arrow').click(function(){
    $("html").animate({ scrollTop: $('html').prop("scrollHeight")}, 1200);
 });
 
+$(document).ready(function(){
+
+  $(window).scroll(function() {
+    drawLine( $('#route'),
+              document.getElementById('path') );
+  });
+  
+  // init the line length
+    drawLine( $('#route'),
+              document.getElementById('path') );
+
+  //draw the line
+  function drawLine(container, line){
+    
+    var pathLength = line.getTotalLength(),
+        maxScrollTop = $(document).height() - $(window).height(),
+        percentDone = $(window).scrollTop() / maxScrollTop,
+        length = percentDone * pathLength;
+    line.style.strokeDasharray = [ length ,pathLength,].join(' ');
+  }
+  
+});
+
